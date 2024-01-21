8e3d3f01525ef4012982d8270c481c5207fc89c3
nataliatesarova <naftalintesarova@gmail.com>
2024-01-21 13:32:47 +0000

    Added product and category models with their attributes

--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254,null=True,blank=True)
+    subcategory = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category',null=True,blank=True,on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254,unique=True)
+    sku = models.CharField(max_length=254, null=True, blank=True, unique=True)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        editable=False)
+    stock_level = models.IntegerField()
+    in_stock = models.BooleanField(default=True)
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=1,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+    
+    
+    
