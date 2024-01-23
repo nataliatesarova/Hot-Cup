60965d59054cf8dcdc6cfce840db4167be05b77d
nataliatesarova <naftalintesarova@gmail.com>
2024-01-23 12:57:06 +0000

    Added product details functionality

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.AllProducts, name='products'),
+    path('<int:product_id>/', views.ProductDetail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category,Product
 
 
@@ -14,3 +14,14 @@ def AllProducts(request):
     
     return render(request, 'products/all_products.html', context)
 
+
+def ProductDetail(request, product_id):
+    """ View for displaying the details of a specific product based on its ID. """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_detail.html', context)
