c3aeadaa1d09f2f55c4ee304ea083b3a1b2e7eb6
nataliatesarova <naftalintesarova@gmail.com>
2024-01-26 11:44:24 +0000

    Add imports for get object or 404 from Django shortcuts and Product from products.models

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,13 +1,26 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
     '''
     Handles the shopping bag contents
     '''
     bag_items = []
-    bag_total = 0
+    total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -15,19 +28,17 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
     
     context = {
         'bag_items': bag_items,
-        'bag_total': total,
+        'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-
-    context = {}
 
     return context
--- bag/urls.py
+++ bag/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ViewShoppingBag, name='view_bag'),
+    path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
 ]
