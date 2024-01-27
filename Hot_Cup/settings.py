618f581d0d2c8e519ded8a4fdbefb7591a5a2de3
nataliatesarova <naftalintesarova@gmail.com>
2024-01-27 14:03:35 +0000

    deployment commit

--- Hot_Cup/settings.py
+++ Hot_Cup/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -31,7 +31,8 @@ DEBUG = False
 
 
 ALLOWED_HOSTS = ['8000-nataliatesarova-hotcup-4hsuztu0d84.ws-eu107.gitpod.io',
-                 'hot-cup-a72a7710ed7c.herokuapp.com']
+                 'hot-cup-a72a7710ed7c.herokuapp.com'
+                 'localhost']
 
 
 # Application definition
@@ -42,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -171,13 +174,16 @@ USE_TZ = True
 #     ]
 # else:
 #     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
