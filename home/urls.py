a03f17ccd9d07f078d8d07ead904c90e467a34d9
nataliatesarova <naftalintesarova@gmail.com>
2024-02-11 17:46:32 +0000

    Adding FAQ section and custom model

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('faq', views.faq, name='faq'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from home.models import FAQ
+
+
 def home(request):
     """ A view to render the home page """
     return render(request, 'home/index.html')
+
+def faq(request):
+    faqs = FAQ.objects.all()
+    return render(request, 'home/faq.html', {'faqs': faqs})
