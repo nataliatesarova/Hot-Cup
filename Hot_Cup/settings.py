1bb7c46ef792a28ca766799dfa9e51983a3c4e79
nataliatesarova <naftalintesarova@gmail.com>
2024-02-17 17:15:03 +0000

    061 sending real emails

--- Hot_Cup/settings.py
+++ Hot_Cup/settings.py
@@ -109,7 +109,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -223,3 +223,15 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 
 STRIPE_PUBLISHABLE_KEY ='pk_test_51OjPyoFlEekqUaJ8BMjHkf0bXlEQwUiiMYLTfAbtztdUiInAYtStXjCBaJaQQQZYyK8HARboSuSUtsX6M84YWrps00gbZm6N81'
 STRIPE_SECRET_KEY ='sk_test_51OjPyoFlEekqUaJ8f5lFGIp26ez9Nn6SdbaauOcsJjPxNtb2CJhrXvca7AV5M9DgU8BBMBhHi4ra1L7IdlG8xq3N00aiSXtJBV'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'naftalintesarova@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
