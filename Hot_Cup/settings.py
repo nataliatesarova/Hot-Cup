8027f516469fe92f2a02f1ea805c1635d75e6f24
nataliatesarova <naftalintesarova@gmail.com>
2024-02-24 15:32:16 +0000

    Updating settings.py

--- Hot_Cup/settings.py
+++ Hot_Cup/settings.py
@@ -221,15 +221,10 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_PUBLISHABLE_KEY =os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY =os.getenv('STRIPE_SECRET_KEY', '')
 
-
-if development:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'hotandcup@example.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
