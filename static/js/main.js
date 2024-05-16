05a3e48cd1d2a1cf6c499c84abf90f00fca83c27
nataliatesarova <naftalintesarova@gmail.com>
2024-05-16 18:47:37 +0000

    Fixing URL in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -36,7 +36,7 @@ async function handleSubmit(e) {
     elements,
     confirmParams: {
       // Make sure to change this to your payment completion page
-      return_url: "https://hot-cup-a72a7710ed7c.herokuapp.com/",
+      return_url: "https://hot-cup-a72a7710ed7c.herokuapp.com/successful",
     },
   });
 
