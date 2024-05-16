a8a5bdc32febe1370a363ad80c946bc873736e9c
nataliatesarova <naftalintesarova@gmail.com>
2024-05-16 18:40:03 +0000

    Checkout page added

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('faq', views.faq, name='faq'),
+    path('successful', views.successful, name='successful')
 ]
--- home/views.py
+++ home/views.py
@@ -7,6 +7,16 @@ def home(request):
     """ A view to render the home page """
     return render(request, 'home/index.html')
 
+
 def faq(request):
     faqs = FAQ.objects.all()
     return render(request, 'home/faq.html', {'faqs': faqs})
+
+
+def successful(request):
+    query_params = request.GET
+    context = {
+        'pi': query_params['payment_intent'],
+        'rs': query_params['redirect_status']
+    }
+    return render(request, 'home/successful_checkout.html', context)
