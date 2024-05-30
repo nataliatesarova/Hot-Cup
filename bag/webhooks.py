ed2c733be151fc593d71c4ed676db6142d4a8d43
nataliatesarova <naftalintesarova@gmail.com>
2024-05-30 12:42:12 +0000

    Correct typo in StripeWHHandler import in webhooks.py

--- bag/webhooks.py
+++ bag/webhooks.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from .webhook_handler import StripeWH_Handler
+from .webhook_handler import StripeWHHandler
 
 
 @require_POST
