e01c570d5ff272465ad1fba7c4d81e9235092181
nataliatesarova <naftalintesarova@gmail.com>
2024-06-04 12:42:23 +0000

    Integrate Stripe payment processing

--- bag/views.py
+++ bag/views.py
@@ -120,23 +120,46 @@ class CheckoutView(View):
         """
         bag = request.session.get('bag', {})
         form_data = {
-            'full_name': request.POST['full_name'],
-            'email': request.POST['email'],
-            'phone_number': request.POST['phone_number'],
-            'country': request.POST['country'],
-            'postcode': request.POST['postcode'],
-            'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
-            'county': request.POST['county'],
+            'full_name': request.POST.get('full_name'),
+            'email': request.POST.get('email'),
+            'phone_number': request.POST.get('phone_number'),
+            'country': request.POST.get('country'),
+            'postcode': request.POST.get('postcode'),
+            'town_or_city': request.POST.get('town_or_city'),
+            'street_address1': request.POST.get('street_address1'),
+            'street_address2': request.POST.get('street_address2'),
+            'county': request.POST.get('county'),
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
+            client_secret = request.POST.get('client_secret')
+            if client_secret:
+                pid = client_secret.split('_secret')[0]
+                order.stripe_pid = pid
+            else:
+                messages.error(request, 'Missing client secret. Please try again.')
+                return redirect(reverse('view_bag'))
+
             order.original_bag = json.dumps(bag)
             order.save()
+
+            # Create Stripe PaymentIntent with metadata
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            intent = stripe.PaymentIntent.create(
+                amount=int(order.grand_total * 100),  # Amount in cents
+                currency=settings.STRIPE_CURRENCY,
+                metadata={
+                    'order_number': order.order_number,
+                    'bag': json.dumps(bag),
+                    'username': request.user.username if request.user.is_authenticated else 'AnonymousUser',
+                },
+            )
+
+            # Save the PaymentIntent id
+            order.stripe_pid = intent.id
+            order.save()
+
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
