66f58395e10985f1a43ccfe186b347c01e9d4de7
nataliatesarova <naftalintesarova@gmail.com>
2024-06-12 13:10:01 +0000

    Fixed view_shopping_bag function reference in urls.py

--- bag/urls.py
+++ bag/urls.py
@@ -5,7 +5,7 @@ from .webhooks import webhook
 
 
 urlpatterns = [
-    path('', views.ViewShoppingBag, name='view_bag'),
+    path('', views.view_shopping_bag, name='view_bag'),
     path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
     path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
     path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
